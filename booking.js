e7e4d60aa4f2c19e3cef782cb43813973264b7f6
PAULA SOFIA ALBI <albi.paula@gmail.com>
2023-03-11 21:02:08 +0800

    dates and sweetAlert

--- booking.js
+++ booking.js
@@ -1,3 +1,16 @@
+let contador= 0;
+let images=  [ "./images/cozy I.jpeg ", "./images/cozy II.jpg" ,"./images/portada.jpg" , "./images/modern-living-room-style.jpg" ]; 
+
+setTimeout( ()  => { progress()  }, 2000 );
+
+
+function progress() {
+    if (contador < 4) {  
+       document.images[0].src=images[contador];
+       contador++;
+       setTimeout("progress()",2000);
+    }
+ }
 /******************************  CLASES  *********************************/
 
 class Reserva {
@@ -16,7 +29,7 @@ class Reserva {
 
     }
 
-let arrayReserva = [];
+let reservaUsuario = [];
 
 if(localStorage.getItem("booking")){
     booking = JSON.parse(localStorage.getItem("booking"));
@@ -67,92 +80,144 @@ arrayDepartamentos.forEach(departamento => {
 /*AGREGAR DATOS A LA RESERVA */
         const boton = document.getElementById(`boton${departamento.personas}`);
 boton.addEventListener("click", () => {
-    
+    Swal.fire({
+        title: 'Seleccionado',
+        icon: 'success',
+        timer: '1500',
+        iconColor:'#995a9fc8',
+        showConfirmButton: false,
+      })
   agregarReserva(departamento.personas);
         })
     })
 }
-
 departamentosOpciones();
-
 /* FUNCION PARA AGREGAR A LA RESERVA EL DPTO SELECCIONADO POR EL USUARIO*/
-const agregarReserva= (personas) => { 
-        const dptoAsignado = arrayDepartamentos.find (departamento => departamento.personas == personas);
-        if (dptoAsignado) {
-        arrayReserva.push(dptoAsignado);
-        console.log(arrayReserva);
+function agregarReserva(personas) {
+    const dptoAsignado = arrayDepartamentos.find(departamento => departamento.personas == personas);
+    if (dptoAsignado) {
+        reservaUsuario.push(dptoAsignado);
+        console.log(reservaUsuario);
+        
         //console.log(dptoAsignado)
-    } 
+    }
+}
 
-/* FUNCION PARA OBTENER DATOS INGRESADOS POR EL USUARIO */
+function calculardiasDiscount() {
+    const timeStart = new Date(document.getElementById("checkIn").value);
+    const timeEnd = new Date(document.getElementById("checkOut").value);
+    const actualDate = new Date();
+    if (timeEnd > timeStart)
+    {
+        const diff = timeEnd.getTime() - timeStart.getTime();
+        document.getElementById("noches").value = Math.round(diff / (1000 * 60 * 60 * 24));
+    }
+    else if (timeEnd != null && timeEnd < timeStart) {
+        Swal.fire ({
+            icon:'warning',
+            text: 'La fecha final de la estadia debe ser mayor a la fecha inicial',
+            width:'500px'
+        });
+        document.getElementById("noches").value = 0;
 
-const formulario = document.getElementById ("formulario"); 
-    formulario.addEventListener ("submit" , (e) =>  { 
+
+    }
+
+}
+
+
+    /* FUNCION PARA OBTENER DATOS INGRESADOS POR EL USUARIO */
+    const formulario = document.getElementById("formulario");
+    formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
 
-        const personas = document.getElementById("personas");                
-        const nombre = document.getElementById("nombre");  
-        const noches = document.getElementById("noches"); 
-        const checkIn = document.getElementById ("checkIn");
-        const checkOut = document.getElementById("checkOut"); 
+        const personas = document.getElementById("personas");
+        const nombre = document.getElementById("nombre");
+        const noches = document.getElementById("noches");
+        const checkIn = document.getElementById("checkIn");
+        const checkOut = document.getElementById("checkOut");
         const solicitud = document.getElementById("solicitud");
+
+        const reserva = new Reserva(personas.value, nombre.value, noches.value, checkIn.value, checkOut.value, solicitud.value);
+        reservaUsuario.push(reserva);
+        formulario.reset();
+        ;
+
+        const booking = JSON.stringify(reservaUsuario);
+
+        console.log(booking);
+        localStorage.setItem("booking", booking);
+        console.log(reservaUsuario);
+        console.log(reserva); 
+
+/*FUNCION QUE CALCULA EL PRECIO TOTAL DE LA RESERVA */ 
+
+function precioTotalAPagar () {
+
+        const totalNoches = (reserva.noches);
+        const precioDpto = (reservaUsuario[0].precioXnoche);
+return (totalNoches * precioDpto);
+
+}
+
+
+
+/* FUNCION PARA AGREGAR RESERVA REALIZADA POR EL CLIENTE AL HTML*/
       
-        const reserva = new Reserva ( personas.value, nombre.value, noches.value, checkIn.value, checkOut.value,solicitud.value); 
-        arrayReserva.push (reserva);
-           // console.log(arrayReserva[1]);
-            console.log ( arrayReserva[0]);
-            console.log (reserva);//line 105 y 107 muestran lo mismo
-            formulario.reset();
-;
-
-const booking = JSON.stringify(arrayReserva);
-   
-    console.log(booking);
-    localStorage.setItem ("booking",booking);
+const reservaContainer = document.getElementById("reserva");
+const verReserva = document.getElementById("formulario");
+        verReserva.addEventListener("click", () => {
+            carrito();
+        });
 
 
-    /* FUNCION PARA AGREGAR RESERVA REALIZADA POR EL CLIENTE AL HTML*/
 
-const reservaContainer = document.getElementById ("reserva");
-const verReserva = document.getElementById ("verReserva");
-
-        verReserva.addEventListener ("click", () => {
-        carrito ();
-    })
+        const carrito = () => {
 
 
-const carrito = () => {
-
-        const div = document.createElement("div");
-        div.className= "container reservaContainer" ;
-        div.innerHTML = `
+            
+            const div = document.createElement("div");
+            div.className = "container reservaContainer";
+            div.innerHTML = `
                         <hr>
                         <div class ="container text-star">
-                           
+                        <h3  >Tu Reserva</h3>
+
+                                <h4  >Tu Reserva</h4>
                                 <h3 class = "text-center"> ${reserva.saludar()}  </h3>
                                 <p> Nombre y Apellido: ${reserva.nombre} </p>
                                 <p> Noches: ${reserva.noches} </p>
                                 <p> CheckIN: ${reserva.checkIn} </p>
                                 <p> CheckOut: ${reserva.checkOut} </p>
                                 <p> Solicitud: ${reserva.solicitud} </p>
-                                <p> Departamento: ${dptoAsignado.nombre} </p>
-                                <p> Precio Noche: $ ${dptoAsignado.precioXnoche} </p>
+                                <p> Departamento: ${reservaUsuario[0].nombre} </p>
+                                <p> Precio Noche: $ ${reservaUsuario[0].precioXnoche} </p>
+                                <p> Precio Total: $ ${precioTotalAPagar()} </p>
                                 <button class = "btn button--seleccion " id="eliminar" > Modificar Reserva</button $>
                                 <button class = "btn button--seleccion" id="eliminar" > Cancelar Reserva</button>
                             
-                        </div>`
-       reservaContainer.appendChild (div);
+                        </div>`;
+            reservaContainer.appendChild(div);
 
-    }})
+        };
+    });
 
-  
-}
+	/* MODIFICAR RESERVA */ 
+// TENGO QUE ACCEDER AL ARRAY 'RESERVAUSUARIO' 0 DEPARTAMENTO 1 PERSONA 
+//TRAER EL FORMULARIO CON LOS DATOS Y LUEGO APRETAR ACEPTAR PARA GUARDAR LOS NUEVOS DATOS 
+//
+
+    /* CANCELAR RESERVA */ 
+
+    //CLEAR EL ARRAY 'RESERVAUSUARIO'
 
 
-//This code defines a function agregarReserva that adds a new booking to an array called arrayReserva. The function first finds a department from an array called arrayDepartamentos that can accommodate the number of personas specified in the booking. If a department is found, the function creates a new Reserva object using the form data submitted by the user and adds it to the arrayReserva. Finally, the function converts the arrayReserva to JSON and stores it in the browser's local storage.
 
-//There is also an event listener attached to a form with ID formulario that listens for form submission. When the form is submitted, it creates a new Reserva object and adds it to the arrayReserva. It then calls the carrito function to display the new booking in the HTML.
+
+//This code defines a function agregarReserva that adds a new booking to an array called reservaUsuario. The function first finds a department from an array called arrayDepartamentos that can accommodate the number of personas specified in the booking. If a department is found, the function creates a new Reserva object using the form data submitted by the user and adds it to the reservaUsuario. Finally, the function converts the reservaUsuario to JSON and stores it in the browser's local storage.
+
+//There is also an event listener attached to a form with ID formulario that listens for form submission. When the form is submitted, it creates a new Reserva object and adds it to the reservaUsuario. It then calls the carrito function to display the new booking in the HTML.
 
 //The carrito function creates a new div element and appends it to the reservaContainer element in the HTML. The div displays the details of the new booking, including the department assigned to the booking and the price per night for that department. There are also two buttons that allow the user to modify or cancel the booking.
 
